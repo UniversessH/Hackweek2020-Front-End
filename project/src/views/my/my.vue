678392b35b2326320f73eb359e0957a89a31ee64
jiangsiying <1337714671@qq.com>
2020-12-11 19:35:56 +0800

    ‘”我的页面大致布局

--- project/src/views/my/my.vue
+++ project/src/views/my/my.vue
@@ -1,16 +1,74 @@
 <!-- 我的页面 -->
 <template>
   <div>
-    <h1>我的</h1>
+    <van-cell-group
+      border="true"
+    >
+      <van-cell
+        title="单元格"
+        value="内容"
+        center
+      >
+        <van-image
+          slot="icon"
+          width="3rem"
+          height="3rem"
+          fit="cover"
+          round
+          :src="require('@/assets/头像/4.jpg')"
+        />
+        <div slot="title">如爱消</div>
+        <div slot="label">美食大玩家</div>
+        <van-button size="43rem">
+          <van-icon name="setting-o" />
+        </van-button>
+      </van-cell>
+      <van-grid>
+        <van-grid-item>
+          <div slot="text">
+            <div class="num">543</div>
+            <div class="text">测评</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <div class="num">57</div>
+            <div class="text">赞同</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <div class="num">24</div>
+            <div class="text">收藏</div>
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <div slot="text">
+            <div class="num">24</div>
+            <div class="text">粉丝</div>
+          </div>
+        </van-grid-item>
+      </van-grid>
+    </van-cell-group>
+    <van-tabs v-model="active" animated>
+      <van-tab title="勋章墙" name="test"></van-tab>
+      <van-tab title="创作中心" name="square"></van-tab>
+      <van-tab title="照片墙" name="follow"></van-tab>
+    </van-tabs>
+    <app-down></app-down>
+    <router-view />
   </div>
 </template>
 
 <script>
+import AppDown from '../layout/components/down'
 export default {
   components: {
+    AppDown
   },
   data () {
     return {
+      active: 0
     }
   },
   methods: {
@@ -18,3 +76,13 @@ export default {
   }
 }
 </script>
+<style scoped>
+.num {
+  font-size: 15px;
+  text-align: center;
+}
+.text {
+  font-size: 16px;
+  text-align: center;
+}
+</style>
